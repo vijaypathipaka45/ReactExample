229a25ddecdb08065406bc93a104a589830dd48b
vijaypathipaka45 <vijaypathipaka45@gmail.com>
2019-12-04 22:25:45 +0530

    added react pdf

---
+++ src/components/models/ReactPdf.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ReactPdf extends React.Component{
+
+    render(){
+
+        return(
+            <div>
+                <h1>ReactPdf file</h1>
+                {/* <ReactPDF
+  file={{
+    url: 'http://localhost:8080/pdfreport'
+  }}
+/> */}
+            </div>
+        )
+    }
+}
+
+export default ReactPdf;
